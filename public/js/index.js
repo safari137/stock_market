6285f8065eb9956606916344839689a4fb991a75
safari137 <dbdill137@gmail.com>
2016-03-26 19:53:52 +0000

    added ability to add stock by pressing enter; 'stock name' clears and re-focuses after being added;

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,27 @@
 var socketClient = new Socket();
 
+var nameElement = $("#name");
+
 $("#addStock").on("click", function() {
-    var symbol = $("#name").val();
+    addSymbol();
+});
+
+nameElement.keypress(function(info) {
+    var enterKey = 13;
+    
+    if (info.charCode === enterKey) {
+        addSymbol();    
+    }
+})
+
+function addSymbol() {
+   var symbol = nameElement.val();
+    
+    nameElement.val('').focus();
     
     $("#error").addClass("invisible");
-    socketClient.send("addStock", {symbol: symbol});
-});
+    socketClient.send("addStock", {symbol: symbol}); 
+}
   
 $(".stocks").on('click', '.delete', function() {
     var id = $(this).parent().attr('id');
